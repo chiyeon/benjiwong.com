d12c027c6f1c4e9579d85e56cb1ae6aabea8c9cb
Benjamin Wong <me@benjiwong.com>
2025-04-11 23:22:26 -0700

    progress

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,12 @@
 module.exports = function(cfg) {
+   cfg.addPassthroughCopy("./src/assets")
+
    cfg.addPassthroughCopy("./src/assets/css/")
    cfg.addWatchTarget("./src/css/")
 
    cfg.addPassthroughCopy("./src/assets/imgs")
 
+   cfg.addPassthroughCopy("./src/resume.pdf")
 
    return {
       dir: {
